c88eb6cd46477a20f8cf7335e39cea35476b8c67
Sami <153022356+Sami-CT@users.noreply.github.com>
2024-12-28 21:49:48 +0100

    Update ServiceVente.scala
    Ajout fonctions DataFrame

--- src/main/scala/sda/traitement/ServiceVente.scala
+++ src/main/scala/sda/traitement/ServiceVente.scala
@@ -15,6 +15,7 @@ object ServiceVente {
 
     def calculTTC () : DataFrame ={
       /*..........................coder ici...............................*/
+      dataFrame.withColumn("TTC", round(col("HTT") + col("HTT") * col("TVA"), 2)).drop("TVA", "HTT")
     }
     def extractDateEndContratVille(): DataFrame = {
       val schema_MetaTransaction = new StructType()
@@ -23,11 +24,16 @@ object ServiceVente {
       val schema = new StructType()
         .add("MetaTransaction", ArrayType(schema_MetaTransaction), true)
       /*..........................coder ici...............................*/
-
+      dataFrame.withColumn("metaData_json", from_json(col("MetaData"), schema))
+        .withColumn("Ville", col("metaData_json.MetaTransaction")(0)("Ville"))
+        .withColumn("Date_End_contrat_raw", col("metaData_json.MetaTransaction")(0)("Date_End_contrat"))
+        .withColumn("Date_End_contrat", regexp_extract(col("Date_End_contrat_raw"), "(\\d{4}-\\d{2}-\\d{2})", 1))
+        .drop("MetaData", "metaData_json", "Date_End_contrat_raw")
     }
 
     def contratStatus(): DataFrame = {
       /*..........................coder ici...............................*/
+      dataFrame.withColumn("Contrat_Status", when(to_date(col("Date_End_contrat"), "yyyy-MM-dd") < current_date(), "Expired").otherwise("Actif"))
     }
 
 
